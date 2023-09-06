81fd086e75b477f701a66c895b33037e9bc6526e
Scabra <karpinskiy.nike@gmail.com>
2023-09-06 15:25:56 +0300

    test 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] argv) {
-        System.out.println("Hello world");
+        System.out.println("hii");
         System.out.println("Karpinskiy Serhii ... ");
     }
 }
