2f9c5e38d2126717df37a9448811baae3bdcc8db
MrScabra <114917287+MrScabra@users.noreply.github.com>
2023-09-06 22:03:56 +0300

    test Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] argv) {
         System.out.println("Hello world");
         System.out.println("Karpinskiy Serhii ... ");
-        //hi
+        //hi its me
     }
 }
