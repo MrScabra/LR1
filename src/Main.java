03e7017edb7e766eebd4feff8c87d483574e6397
Scabra <karpinskiy.nike@gmail.com>
2023-09-13 23:20:07 +0300

    test 7

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] argv) {
         System.out.println("Hello world");
-        System.out.println("Karpinskiy Serhii ... ");
-        //hi it's me
-        //cool nice
+        System.out.println("Karpinskiy Serhii PZ-20-1/9 ");
+
+        // Отримання поточної дати та часу
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        // Форматування дати та часу в рядок
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = currentDateTime.format(formatter);
+
+        System.out.println("Поточна дата і час: " + formattedDateTime);
     }
 }
